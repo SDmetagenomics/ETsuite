cd18bd18cb115f6c179385610894a38ec0da1192
alexcritschristoph <acritsc1@jhu.edu>
2019-11-12 18:41:57 -0800

    Updating bam_pe_stats to include QUALNM, QUAL, and CLIPPED

--- scripts/bam_pe_stats.py
+++ scripts/bam_pe_stats.py
@@ -31,11 +31,40 @@ def read_bam(scaf2bin, bam):
                                 strand = '+'
 
                             if read.query_name not in read_info:
+                                    qseq = read.query_sequence
+                                    refseq = read.get_reference_sequence()
+                                    good_pos = 0
+                                    true_nm = 0
+                                    clipped = 'False'
+                                    if len(qseq) == len(refseq):
+                                        for pos in range(0,len(read.query_alignment_qualities)):
+                                            if read.query_alignment_qualities[pos] > 20:
+                                                good_pos += 1
+                                                if qseq[pos] != refseq[pos]:
+                                                    true_nm += 1
+                                    else:
+                                        clipped = 'True'
+
                                     read_info[read.query_name] = {"scaf": scaf, 'is_read1': read.is_read1, 'strand': strand, "mapq": read.mapping_quality, "NM": read.get_tag("NM"), 
-                                    "len": len(read.get_reference_positions()), "end": read.get_reference_positions()[-1], "start": read.get_reference_positions()[0]}
+                                    "len": len(read.get_reference_positions()), "end": read.get_reference_positions()[-1], "start": read.get_reference_positions()[0],
+                                    'good_pos':good_pos, 'true_nm': true_nm, 'clipped': clipped}
                             else:
                                     found_both.add(read.query_name)
                                     if read.is_read1:
+                                            qseq = read.query_sequence
+                                            refseq = read.get_reference_sequence()
+                                            good_pos = 0
+                                            true_nm = 0
+                                            clipped = 'False'
+                                            if len(qseq) == len(refseq):
+                                                for pos in range(0,len(read.query_alignment_qualities)):
+                                                    if read.query_alignment_qualities[pos] > 20:
+                                                        good_pos += 1
+                                                        if qseq[pos] != refseq[pos]:
+                                                            true_nm += 1
+                                            else:
+                                                clipped = 'True'
+
                                             reads['Read'].append(read.query_name)
                                             reads['SCAF1'].append(scaf)
                                             reads['GENOME1'].append(scaf2bin[scaf])
@@ -45,6 +74,9 @@ def read_bam(scaf2bin, bam):
                                             reads['START1'].append(read.get_reference_positions()[0])
                                             reads['END1'].append(read.get_reference_positions()[-1])
                                             reads['STRAND1'].append(strand)
+                                            reads['QUAL1'].append(good_pos)
+                                            reads['QUALNM1'].append(true_nm)
+                                            reads['CLIPPED1'].append(clipped)
 
                                             reads['SCAF2'].append(read_info[read.query_name]['scaf'])
                                             reads['GENOME2'].append(scaf2bin[read_info[read.query_name]['scaf']])
@@ -54,8 +86,26 @@ def read_bam(scaf2bin, bam):
                                             reads['START2'].append(read_info[read.query_name]['start'])
                                             reads['END2'].append(read_info[read.query_name]['end'])
                                             reads['STRAND2'].append(read_info[read.query_name]['strand'])
+                                            reads['QUAL2'].append(read_info[read.query_name]['good_pos'])
+                                            reads['QUALNM2'].append(read_info[read.query_name]['true_nm'])
+                                            reads['CLIPPED2'].append(read_info[read.query_name]['clipped'])
 
                                     elif read.is_read2:
+                                            qseq = read.query_sequence
+                                            refseq = read.get_reference_sequence()
+                                            good_pos = 0
+                                            true_nm = 0
+                                            clipped = 'False'
+                                            if len(qseq) == len(refseq):
+                                                for pos in range(0,len(read.query_alignment_qualities)):
+                                                    if read.query_alignment_qualities[pos] > 20:
+                                                        good_pos += 1
+                                                        if qseq[pos] != refseq[pos]:
+                                                            true_nm += 1
+                                            else:
+                                                clipped = 'True'
+
+
                                             reads['Read'].append(read.query_name)
                                             reads['SCAF2'].append(scaf)
                                             reads['GENOME2'].append(scaf2bin[scaf])
@@ -65,6 +115,9 @@ def read_bam(scaf2bin, bam):
                                             reads['START2'].append(read.get_reference_positions()[0])
                                             reads['END2'].append(read.get_reference_positions()[-1])
                                             reads['STRAND2'].append(strand)
+                                            reads['QUAL2'].append(good_pos)
+                                            reads['QUALNM2'].append(true_nm)
+                                            reads['CLIPPED2'].append(clipped)
 
                                             reads['SCAF1'].append(read_info[read.query_name]['scaf'])
                                             reads['GENOME1'].append(scaf2bin[read_info[read.query_name]['scaf']])
@@ -74,6 +127,9 @@ def read_bam(scaf2bin, bam):
                                             reads['START1'].append(read_info[read.query_name]['start'])
                                             reads['END1'].append(read_info[read.query_name]['end'])
                                             reads['STRAND1'].append(read_info[read.query_name]['strand'])
+                                            reads['QUAL1'].append(read_info[read.query_name]['good_pos'])
+                                            reads['QUALNM1'].append(read_info[read.query_name]['true_nm'])
+                                            reads['CLIPPED1'].append(read_info[read.query_name]['clipped'])
 
 
 
@@ -90,6 +146,9 @@ def read_bam(scaf2bin, bam):
                 reads['START1'].append(read_info[read]['start'])
                 reads['END1'].append(read_info[read]['end'])
                 reads['STRAND1'].append(read_info[read]['strand'])
+                reads['QUAL1'].append(read_info[read]['good_pos'])
+                reads['QUALNM1'].append(read_info[read]['true_nm'])
+                reads['CLIPPED1'].append(read_info[read]['clipped'])
 
                 reads['SCAF2'].append('NA')
                 reads['GENOME2'].append('NA')
@@ -99,6 +158,10 @@ def read_bam(scaf2bin, bam):
                 reads['START2'].append('NA')
                 reads['END2'].append('NA')
                 reads['STRAND2'].append('NA')
+                reads['QUAL2'].append('NA')
+                reads['QUALNM2'].append('NA')
+                reads['CLIPPED2'].append('NA')
+
             else:
                 reads['Read'].append(read)
                 reads['SCAF2'].append(read_info[read]['scaf'])
@@ -109,6 +172,9 @@ def read_bam(scaf2bin, bam):
                 reads['START2'].append(read_info[read]['start'])
                 reads['END2'].append(read_info[read]['end'])
                 reads['STRAND2'].append(read_info[read]['strand'])
+                reads['QUAL2'].append(read_info[read]['good_pos'])
+                reads['QUALNM2'].append(read_info[read]['true_nm'])
+                reads['CLIPPED2'].append(read_info[read]['clipped'])
 
                 reads['SCAF1'].append('NA')
                 reads['GENOME1'].append('NA')
@@ -118,6 +184,9 @@ def read_bam(scaf2bin, bam):
                 reads['START1'].append('NA')
                 reads['END1'].append('NA')
                 reads['STRAND1'].append('NA')
+                reads['QUAL1'].append('NA')
+                reads['QUALNM1'].append('NA')
+                reads['CLIPPED1'].append('NA')
 
     pd.DataFrame(reads).to_csv(sys.stdout, index=False, sep="\t")
     return True
