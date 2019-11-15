f45c6b7f8d9e85f18ebc2b47fdaa59619d1dc40e
SDmetagenomics <spncrdmnd@gmail.com>
2019-11-14 18:08:24 -0800

    ETstats v0.02 ; read stats w/ plotting

--- R_code/ETstats.R
+++ R_code/ETstats.R
@@ -14,13 +14,24 @@ if ("dplyr" %in% installed.packages() == FALSE){
   q(save="no")
 }
 
+# Check ggplot2
+if ("ggplot2" %in% installed.packages() == FALSE){
+  print("Please install R package ggplot2. Program quitting...")
+  q(save="no")
+}
+
+# Check ggforce
+if ("ggforce" %in% installed.packages() == FALSE){
+  print("Please install R package ggforce. Program quitting...")
+  q(save="no")
+}
 
 
 ### Load packages
 suppressMessages(library(data.table, quietly = T))
 suppressMessages(library(dplyr, quietly = T))
 suppressMessages(library(ggplot2, quietly = T))
-
+suppressMessages(library(ggforce, quietly = T))
 
 
 ### Set up path variables for associated scripts and databases
@@ -48,7 +59,7 @@ cat(paste0("\nTotal Arguments: ",length(args),"\n"))
 ## Display help if no args or -h
 if("-h" %in% args | !("-w" %in% args) | !("-d" %in% args) | !("-b" %in% args) | length(args) == 0) {
   cat("
-      ETstats v0.01
+      ETstats v0.02
       
       Usage: ETstats.R -w [workflow] -d [ETmapper_out_dir] -b [batch_file] [options]
 
@@ -66,7 +77,7 @@ if("-h" %in% args | !("-w" %in% args) | !("-d" %in% args) | !("-b" %in% args) | 
       -g: Directory containing genome database (No Default)
       -o: Output file (Default: ETstats_out.txt)
       -p: Make plots (Default: FALSE)
-      -r: Remove specific genome from analysis
+      -r: Remove specific genome from analysis (must be quoted)
       
       Stats Summary Options:
       
@@ -93,7 +104,7 @@ if("-h" %in% args | !("-w" %in% args) | !("-d" %in% args) | !("-b" %in% args) | 
 
 # Work Flow Type
 wf <- args[which(args == "-w") + 1]
-if(wf %notin% c("ss","ip")){
+if(wf %notin% c("ss","rs","ip")){
   cat(paste0("\n",wf," is not a kown workflow...exiting"))
   q(save="no")
 }
@@ -147,19 +158,18 @@ if("-m" %in% args){
   mm_cut <- as.numeric(args[which(args == "-m") + 1])
 }
 
-# MCustom filter function (Overrides PE filters)
+# Custom filter function (Overrides PE filters)
 if("-C" %in% args){
   custom_filt <- args[which(args == "-C") + 1]
-  
 }
 
 
 
 
 
-## Load Testing Data 
-# batch_file <- read.table("test_data/ETmapper/test_batch.txt", header = F, sep = "\t", stringsAsFactors = F)
-# em_out <- "test_data/ETmapper/full_paired_end_test/"
+# Load Testing Data
+#batch_file <- read.table("test_data/ETmapper/test_batch.txt", header = F, sep = "\t", stringsAsFactors = F)
+#em_out <- "test_data/ETmapper/full_paired_end_test/"
 # pe_example <- fread("test_data/ETmapper/paired_end_test/hits/JD_ZZ_2ndcycle_1.hits")
 # se_example <- fread("test_data/ETmapper/single_end_test/hits/JD_ZZ_2ndcycle_1.hits")
 # 
@@ -251,7 +261,7 @@ hit.summary.pe <- function(){
                                               MMM1_RAW = mean(NM1,na.rm = T),
                                               MLEN1_RAW = mean(LEN1,na.rm = T),
                                               MQUAL1_RAW = mean(QUAL1,na.rm = T),
-                                              MQUALNM1 = mean(QUALNM1,na.rm = T),
+                                              MQUALNM1_RAW = mean(QUALNM1,na.rm = T),
                                               MMQ2_RAW = mean(MAPQ2,na.rm = T),
                                               MMM2_RAW = mean(NM2,na.rm = T),
                                               MLEN2_RAW = mean(LEN2,na.rm = T),
@@ -355,22 +365,140 @@ hit.summary.se <- function(){
 ## Function 4: Basic Plotting of genomes (genomes v sample groups)
   
 
-## Function 5: Comparative Statistics between groups   
+## Function 5: Read summary stats ### SPLIT THIS FUNCTION EVENTUALLY FOR READABLITY
+read.stats <- function(){
   
+  # Make df and list to hold summary and full hit tables
+  #aggregate_hits <- data.table() --- Don't do this right now
+  read_summary <- data.table()
+  
+  # loop over batch file depending on data type
+  if (paired_end_data == TRUE){
+    
+    # Build master df of all hits
+    for (i in 1:nrow(batch_file)){
+      
+      # Read in hit table
+      tmp_hit_table <- fread(paste0(em_out,"hits/",batch_file[i,1],".hits"), sep = "\t")
+      
+      # Remove genomes if in arguments
+      if (exists("rm_gen") == TRUE){
+        tmp_hit_table <- subset(tmp_hit_table, GENOME1 %notin% rm_gen[[1]] & GENOME2 %notin% rm_gen[[1]])
+        
+      }
+        
+      # Sumarize raw data on reads
+      tmp_raw_summary <- data.table(SAMPLE = batch_file[i,1],
+                                    GROUP = batch_file[i,2],
+                                    tmp_hit_table %>% 
+                                      summarise(READ_RAW = n(),
+                                                UNIQ_RAW = n_distinct(barcodes),
+                                                BPR_RAW = n_distinct(barcodes) / n(),
+                                                MMQ1_RAW = mean(MAPQ1,na.rm = T),
+                                                MMM1_RAW = mean(NM1,na.rm = T),
+                                                MLEN1_RAW = mean(LEN1,na.rm = T),
+                                                MQUAL1_RAW = mean(QUAL1,na.rm = T), # QUAL = number of bases above phred20
+                                                MQUALNM1_RAW = mean(QUALNM1,na.rm = T), #QUALNM = number of mismatches ONLY at bases above phred20
+                                                MANI1_RAW = mean(NM1 / LEN1, na.rm = T), # raw ANI = number of differences including errors
+                                                MHQANI1_RAW = mean(QUALNM1 / QUAL1, na.rm = T), # high qual ANI = number of differences mostly not including errors
+                                                MPQ1_RAW = mean(QUAL1 / LEN1, na.rm = T), # percent quality = the percentage of high quality bases on the read
+                                                MMQ2_RAW = mean(MAPQ2,na.rm = T),
+                                                MMM2_RAW = mean(NM2,na.rm = T),
+                                                MLEN2_RAW = mean(LEN2,na.rm = T),
+                                                MQUAL2_RAW = mean(QUAL2,na.rm = T), # QUAL = number of bases above phred20
+                                                MQUALNM2_RAW = mean(QUALNM2,na.rm = T), # QUALNM = number of mismatches ONLY at bases above phred20
+                                                MANI2_RAW = mean(NM2 / LEN2, na.rm = T), # raw ANI = number of differences including errors
+                                                MHQANI2_RAW = mean(QUALNM2 / QUAL2, na.rm = T), # high qual ANI = number of differences mostly not including errors
+                                                MPQ2_RAW = mean(QUAL2 / LEN2, na.rm = T))) # percent quality = the percentage of high quality bases on the read
+      
+      # Create taable of read summary statistics for each sample
+      read_summary <- rbind(read_summary, tmp_raw_summary)
+      
+    } ## End looping over PE files
+     
+    
+    # Plot Summary Data if plotting == T in args
+    if (plot_res == TRUE){
+      
+      # Melt data for plotting
+      read_summary_plot <- melt(read_summary)
+      
+      # Produce ggplot
+      ggplot(read_summary_plot, aes(x = GROUP, y = value, color = GROUP)) +
+        geom_point() +
+        #geom_boxplot() +
+        facet_wrap(. ~ variable, scales = "free_y")
+      
+      ggsave("read_stats.pdf", device = "pdf", width = 20, height = 20)
+       
+    } 
+    
+  } ## End paired end summmary analysis
+  
+  
+  
+  if (paired_end_data == FALSE){
+   
+    # Build master df of all hits
+    for (i in 1:nrow(batch_file)){
+      
+      # Read in hit table
+      tmp_hit_table <- fread(paste0(em_out,"hits/",batch_file[i,1],".hits"), sep = "\t")
+      
+      # Remove genomes if in arguments
+      if (exists("rm_gen") == TRUE){
+        tmp_hit_table <- subset(tmp_hit_table, GENOME1 %notin% rm_gen[[1]])
+        
+      }
+      
+      # Sumarize raw data
+      tmp_raw_summary <- data.table(SAMPLE = batch_file[i,1],
+                                    GROUP = batch_file[i,2],
+                                    tmp_hit_table %>% 
+                                      summarise(READ_RAW = n(),
+                                                UNIQ_RAW = n_distinct(barcodes),
+                                                BPR_RAW = n_distinct(barcodes) / n(),
+                                                MMQ_RAW = mean(MAPQ),
+                                                MMM_RAW = mean(NM),
+                                                MLEN_RAW = mean(LEN)))
+      
+      # Create taable of read summary statistics for each sample
+      read_summary <- rbind(read_summary, tmp_raw_summary)
+      
+    } ## End looping over PE files
+    
+    
+    # Plot Summary Data if plotting == T in args
+    if (plot_res == TRUE){
+      
+      # Melt data for plotting
+      read_summary_plot <- melt(read_summary)
+      
+      # Produce ggplot
+      ggplot(read_summary_plot, aes(x = GROUP, y = value, color = GROUP)) +
+        geom_point() +
+        #geom_boxplot() +
+        facet_wrap(. ~ variable, scales = "free_y")
+      
+      ggsave("read_stats.pdf", device = "pdf", width = 20, height = 20)
+      
+    }
+    
+  } ## End SE summmary analysis
 
-## Function 6: Read Stats!!!
+  
+  # Return Summary Data 
+  read_summary
+  
+} ## End read.stats function
 
 
 
-# raw ANI = NM / LEN
-# high qual ANI = QUALNM / QUAL
-# percent_read_quality = QUAL / LEN
-# raw ANI = number of differences including errors
-# high qual ANI = number of differences mostly not including errors
-# percent quality = the percentage of high quality bases on the read
+## Function 6: Comparative Statistics between groups (hits or read summaries)
 
 
 
+#### END FUNCTION DEFINE ####
 
 
 
@@ -399,7 +527,15 @@ if (wf == "ss"){
 
 
 if (wf == "rs"){
+ 
+  # Check if data is pe or se
+  paired_end_data <- pe.test()
   
+  # loop over batch file depending on data type
+  read_stats_out <- read.stats()
+  
+  # Write Output
+  write.table(read_stats_out, file = out_dir, quote = F, row.names = F, sep = "\t")
  
    
 }
@@ -412,100 +548,5 @@ if (wf == "ip"){
 
 
 
-
-
-
-
-
-
-
-
-
-
-# ## ANALYSIS STEP 2 - Create Final DataFrame with All Values 
-# 
-# ## Collect All Unique Genome Names
-# uniq_gen <- vector()
-# 
-# for (i in 1:length(Final_Results)){
-#   tmp_gen <- Final_Results[[i]][[1]]
-#   uniq_gen <- c(uniq_gen, tmp_gen)
-# } 
-# 
-# uniq_gen <- unique(uniq_gen)
-# 
-# 
-# ## Combine Metadata and count summaries (includes every genome per sample)
-# Hit_Summary <- data.frame()
-# 
-# for (i in 1:length(Final_Results)){
-#   
-#   tmp_meta <- data.frame(Genome = uniq_gen,
-#                          Sample = batch_file$sample_name[i],
-#                          Group = batch_file$group[i])
-#   
-#   tmp_merge <- merge(tmp_meta, Final_Results[[i]], by = "Genome", all.x = T)
-#   
-#   # Order dataframe by Uniq_Filt 
-#   tmp_merge <- tmp_merge[order(tmp_merge$Uniq_Filt, decreasing = T),]
-#   
-#   # Remove NAs and NaN from data
-#   #tmp_merge[is.na(tmp_merge)] <- 0
-#   
-#   # Concatenate rows into final master dataframe
-#   Hit_Summary <- rbind(Hit_Summary, tmp_merge)
-#   
-# }
-# 
-# 
-# ## Write Count Summary Data
-# #out_dir <- getwd()
-# write.table(Hit_Summary, file = "../Studies/19_11_5_JD_ZZ_Diversity_A/Hit_Summary.txt", quote = F, row.names = F, sep = "\t")
-# 
-# 
-
-
-
-
-
-
-
-
-
-
-
-# ##### MANUAL SHIT #####
-# 
-# 
-# hit_table <- fread("../Studies/19_11_5_JD_ZZ_Diversity_A/JD_ZZ_diversity_A_1.hits2", sep = "\t")
-# hit_table_filt <- subset(hit_table, MAPQ >= 20)
-# 
-# counts <- count(hit_table$GENOME)
-# 
-# kleb_counts_filt <- subset(hit_table, GENOME == "Klebsiella_michiganensis_M5a1_GCF_002090195.1" & MAPQ >= 20)
-# 
-# kleb_barcode <- length(unique(kleb_counts_filt$barcodes)) - 1
-# 
-# 
-# 
-# unique_barcodes <- data.frame(sample_size = seq(100000, 1100000, by = 50000),
-#                               unique = 1:21)
-# sample_vec <- c(1:nrow(hit_table_filt))
-# 
-# for (i in 1:21){
-#   tmp_select <- sample(sample_vec, size = unique_barcodes[i,1], replace = F)
-#   
-#   tmp_hits <- hit_table_filt[tmp_select,]
-#   
-#   tmp_barcode_uniq <- length(unique(tmp_hits$barcodes)) - 1
-#   
-#   unique_barcodes[i,2] <- tmp_barcode_uniq
-# }
-# 
-# 
-# ggplot(unique_barcodes, aes(x = sample_size, y = unique)) +
-#   geom_point() +
-#   geom_smooth() +
-#   geom_abline(slope = 1, linetype = 2)
 
 
