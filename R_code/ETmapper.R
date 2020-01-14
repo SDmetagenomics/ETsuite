2b1300c4209289891e06432307ae48ce3afae876
SDmetagenomics <spncrdmnd@gmail.com>
2020-01-14 13:08:04 -0800

    stats pull test 5

--- R_code/ETmapper.R
+++ R_code/ETmapper.R
@@ -279,17 +279,16 @@ pull.run.stats <- function(){
       
       # Say what is happening
       cat(paste0("\nAggregating ETmapper stats for: ",batch_file[i,1],"\n"))
+      cat(nrow(batch_file))
       
       # Pull stats from logs
       if(paired_end_data == TRUE){
-        #lm_workflow_stats[i,5] <- as.numeric(system(paste0("grep 'Total read pairs processed:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'"), intern = T))
-        cat(paste0("grep 'Total read pairs processed:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'\n"))
-        foo <- as.numeric(system(paste0("grep 'Total read pairs processed:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'"), intern = T))
-        cat(foo)
+        lm_workflow_stats[i,5] <- as.numeric(system(paste0("grep 'Total read pairs processed:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'"), intern = T))
         lm_workflow_stats[i,6] <- as.numeric(system(paste0("grep 'Read 1 with adapter:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'"), intern = T))
         lm_workflow_stats[i,8] <- as.numeric(system(paste0("grep 'Read 2 with adapter:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'"), intern = T))
         lm_workflow_stats[i,10] <- as.numeric(system(paste0("grep 'Pairs written:' ",out_dir,"/logs/",batch_file[i,1],".trim.log ","| awk '{print $5}' | sed 's/,//'"), intern = T))
         lm_workflow_stats[i,12] <- as.numeric(system(paste0("sed 1d ",out_dir,"/hits/",batch_file[i,1],".mghits | wc -l"), intern = T))
+        print(lm_workflow_stats)
       }
       
       if(paired_end_data == FALSE){
