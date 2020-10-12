aa2d23982c5a3798222d503d5b62fc6b749b6cc0
SDmetagenomics <spncrdmnd@gmail.com>
2020-10-12 15:36:45 -0700

    updated check requirements

--- scripts/check_requirements.R
+++ scripts/check_requirements.R
@@ -8,18 +8,36 @@ system("samtools --version")
 
 print("Checking cutadapt (>=2.6)")
 if(as.numeric(system('cutadapt --version', intern=TRUE)) >= 2.6){
-	print("cutadapt >= 2.6 found")
+  print("cutadapt >= 2.6 found")
 }
 
 print("Checking bowtie2 (>= 2.3)")
 system('bowtie2 --version')
-	print("bowtie2 found")
 
 print("Checking prodigal")
 system('prodigal -v')
+
+print("Checking Bartender")
+system ('command -v bartender_single_com')
 
 print("Checking pysam (>= 0.15)")
 system('pip3 list | grep pysam')
 
 print("Checking pandas ( >= 0.2)")
 system('pip3 list | grep pandas')
+
+print("Checking SeqIO")
+system('pip3 list | grep biopython')
+
+print("Checking R Packages")
+needed_packages <- c("data.table", "seqinr", "tools", "stringr", "dplyr", "ggplot2", "MASS")
+found_all <- TRUE
+for(package in needed_packages){
+  if(!(package %in% installed.packages())){
+    print(paste("package", package, "not found", sep = " "))
+    found_all <- FALSE
+  }
+}
+if(found_all){
+  print("All R packages installed")
+}
